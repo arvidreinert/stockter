8d8aa0c66ee6424e70055cf535dcc7592d230405
Arvid Reinert <168847324+arvidreinert@users.noreply.github.com>
2025-12-20 15:05:19 +0100

    finished a test design

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 import sys
 import PySide6.QtWidgets as pysdw
 from PySide6.QtGui import QPixmap
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QFont
 
+#fonts:
+titlefont = QFont()
+titlefont.setFamily("Bauhaus 93")
+titlefont.setPointSize(18)
+titlefont.setBold(True)
+titlefont.setItalic(True)
+
+#main font:
+mainfont = QFont()
+mainfont.setFamily("ADLaM Display")
+mainfont.setPointSize(12)
+mainfont.setBold(False)
+mainfont.setItalic(False)
+
+#the app:
 app = pysdw.QApplication(sys.argv)
 
 app.setStyle("Fusion")
@@ -9,18 +26,53 @@ app.setStyleSheet("""
 QWidget {
     background-color: #121212;
     color: #ffffff;
+}
+QLabel {
+    color: #FFD52E;              
 }""")
 
+#window:
 window = pysdw.QWidget() 
-window.setWindowTitle("Stockter")
-window.setWindowIcon("images/icon.WEBP")
+window.setWindowTitle("stockDuck")
+window.setWindowIcon(QPixmap("images/icon.webp"))
+
+#stacks:
+stack = pysdw.QStackedWidget()
+
+#pages:
+homepage = pysdw.QWidget()
+homelayout = pysdw.QVBoxLayout()
+homepage.setLayout(homelayout)
+stack.addWidget(homepage)
+
+testpage = pysdw.QWidget()
+testlayout = pysdw.QVBoxLayout()
+testpage.setLayout(testlayout)
+stack.addWidget(testpage)
 
 #Widgets
-label = pysdw.QLabel("Status: nichts passiert")
+#titlelabel:
+hometitle = pysdw.QLabel("Welcom to stockDuck!")
+hometitle.setAlignment(Qt.AlignHCenter)
+hometitle.setFont(titlefont)
+homelayout.addWidget(hometitle)
+
+homebutton = pysdw.QPushButton("home")
+homebutton.clicked.connect(lambda: stack.setCurrentWidget(homepage))
+testlayout.addWidget(homebutton)
+
+testitle = pysdw.QLabel("Wtest")
+testlayout.addWidget(testitle)
+
+testbutton = pysdw.QPushButton("test")
+testbutton.clicked.connect(lambda: stack.setCurrentWidget(testpage))
+homelayout.addWidget(testbutton)
+
 #Layouts
 main_layout = pysdw.QVBoxLayout()      
 
-main_layout.addWidget(label)
+main_layout.addWidget(stack)
+stack.setCurrentWidget(homepage)
 
 window.setLayout(main_layout)
 window.resize(400, 200)
